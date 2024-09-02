6e71f782b418c72742f1579259f577d360a6aff5
geraldine.rouy <geraldine.rouy@le-campus-numerique.fr>
2024-09-02 16:45:20 +0200

    exo 4 in progress

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,13 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+secretMessage = secretMessage.pop;
+
+secretMessage = secretMessage.push("to");
+
+secretMessage = secretMessage.push("program");
+
+secretMessage.forEach(function (item, index, array) {
+    console.log(item, index);
+})
