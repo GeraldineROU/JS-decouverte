b0103ff604299ad7eec4681fbf750d8efe9dc0e4
geraldine <geraldine.rouy@le-campus-numerique.fr>
2024-09-04 21:51:13 +0200

    exo 7, steps 1,2 and 3 done !

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -1,3 +1,39 @@
 console.log("exo-7");
 
 console.log(jsonDatas);
+
+
+////////afficher chaque article dans la console///////////////////////
+
+function displayAllItems () {
+    jsonDatas.forEach((item) => {
+        console.log("Article :");
+        Object.entries(item).forEach((entry) => {
+            console.log(entry[0] + " : " + entry[1]);
+        })
+        // for (let key in item) {
+        //     console.log(key + " : " + item[key]);
+        // }
+        console.log("-----------------------------------------");
+    });
+}
+
+displayAllItems();
+
+/////////// créer un tableau de traduction des "types" //////////////
+
+const typeTranslation = {
+    car : "voiture",
+    house : "maison",
+    game : "jeu",
+    videoGame : "jeu vidéo",
+    show : "spectacle"
+}
+
+////////////// Ajouter une nouvelle clé aux objets existants pour stocker le typer traduit, sans modifier datas.js ////
+
+jsonDatas.forEach((item) => {
+    item.translatedType = typeTranslation[item.type];
+});
+
+displayAllItems();
