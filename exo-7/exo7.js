4a4821ea45633c95d0040ac0429df67dd8f88efe
geraldine.rouy <geraldine.rouy@le-campus-numerique.fr>
2024-09-05 12:23:31 +0200

    exo 6 in progress, all items displayed in html page

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -37,3 +37,89 @@ jsonDatas.forEach((item) => {
 });
 
 displayAllItems();
+
+///////// Créer l'affichage de tous les article dans la page html //////////////////////////////////
+
+// Main code
+
+// Get container DOM element
+let container = document.getElementById("container");
+
+for (let item of jsonDatas) {
+
+
+
+
+
+    // Create item DOM element from jsonData
+    // explication : prendre la description JS de l'élément et le transformer en element HTML
+
+    let itemNameDomElement = createDomElement("Nom : " + item.name);
+    let itemTypeDomElement = createDomElement("Type : " + item.translatedType);
+    let itemDescriptionDomElement = createDomElement("Description : " + item.description);
+    let itemPriceDomElement = createDomElement("Prix : " + item.price);
+    let itemQuantityDomElement = createDomElement("Quantité : " + item.quantity);
+
+    // append l'élement créé à ton container
+
+    container.appendChild(itemNameDomElement);
+    container.appendChild(itemTypeDomElement);
+    container.appendChild(itemDescriptionDomElement);
+    container.appendChild(itemPriceDomElement);
+    container.appendChild(itemQuantityDomElement);
+
+    let separator = document.createElement("hr");
+    container.appendChild(separator);
+
+}
+
+///////// fonction pour créer un élément DOM en reprenant les entrées de jsonDatas
+
+function createDomElement (dataText) {
+    let element = document.createElement("p");
+    element.textContent = dataText;
+    return element;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
