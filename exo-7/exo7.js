d765ccacae89667b3e3bd418a863e6c003948d5c
geraldine.rouy <geraldine.rouy@le-campus-numerique.fr>
2024-09-05 16:43:21 +0200

    exo 6 in progress, search field added and displays items by type

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -5,7 +5,7 @@ console.log(jsonDatas);
 
 ////////afficher chaque article dans la console///////////////////////
 
-function displayAllItems () {
+function displayAllItemsInConsole () {
     jsonDatas.forEach((item) => {
         console.log("Article :");
         Object.entries(item).forEach((entry) => {
@@ -18,7 +18,7 @@ function displayAllItems () {
     });
 }
 
-displayAllItems();
+displayAllItemsInConsole();
 
 /////////// créer un tableau de traduction des "types" //////////////
 
@@ -36,7 +36,7 @@ jsonDatas.forEach((item) => {
     item.translatedType = typeTranslation[item.type];
 });
 
-displayAllItems();
+displayAllItemsInConsole();
 
 ///////// Créer l'affichage de tous les article dans la page html //////////////////////////////////
 
@@ -45,33 +45,33 @@ displayAllItems();
 // Get container DOM element
 let container = document.getElementById("container");
 
-for (let item of jsonDatas) {
+function displayAllItems () {
+    for (let item of jsonDatas) {
 
+        // Create item DOM element from jsonData
+        // explication : prendre la description JS de l'élément et le transformer en element HTML
 
+        let itemNameDomElement = createDomElement("Nom : " + item.name);
+        let itemTypeDomElement = createDomElement("Type : " + item.translatedType);
+        let itemDescriptionDomElement = createDomElement("Description : " + item.description);
+        let itemPriceDomElement = createDomElement("Prix : " + item.price);
+        let itemQuantityDomElement = createDomElement("Quantité : " + item.quantity);
 
+        // append l'élement créé à ton container
 
+        container.appendChild(itemNameDomElement);
+        container.appendChild(itemTypeDomElement);
+        container.appendChild(itemDescriptionDomElement);
+        container.appendChild(itemPriceDomElement);
+        container.appendChild(itemQuantityDomElement);
 
-    // Create item DOM element from jsonData
-    // explication : prendre la description JS de l'élément et le transformer en element HTML
+        let separator = document.createElement("hr");
+        container.appendChild(separator);
 
-    let itemNameDomElement = createDomElement("Nom : " + item.name);
-    let itemTypeDomElement = createDomElement("Type : " + item.translatedType);
-    let itemDescriptionDomElement = createDomElement("Description : " + item.description);
-    let itemPriceDomElement = createDomElement("Prix : " + item.price);
-    let itemQuantityDomElement = createDomElement("Quantité : " + item.quantity);
-
-    // append l'élement créé à ton container
-
-    container.appendChild(itemNameDomElement);
-    container.appendChild(itemTypeDomElement);
-    container.appendChild(itemDescriptionDomElement);
-    container.appendChild(itemPriceDomElement);
-    container.appendChild(itemQuantityDomElement);
-
-    let separator = document.createElement("hr");
-    container.appendChild(separator);
-
+    }
 }
+
+// displayAllItems();
 
 ///////// fonction pour créer un élément DOM en reprenant les entrées de jsonDatas
 
@@ -80,6 +80,46 @@ function createDomElement (dataText) {
     element.textContent = dataText;
     return element;
 }
+
+
+////////////////Au clic sur le bouton "rechercher", afficher les articles du type entré dans le champs texte /////
+
+////// récupérer le bouton dans le dom
+let button = document.getElementById("button")
+
+
+////////faire une fonction A qui permet d'afficher les articles associés à un type
+
+function displayItemsByType(typeSearched) {
+        for (let item of jsonDatas) {
+            if (item.translatedType === typeSearched) {
+                let itemNameDomElement = createDomElement("Nom : " + item.name);
+                let itemTypeDomElement = createDomElement("Type : " + item.translatedType);
+                let itemDescriptionDomElement = createDomElement("Description : " + item.description);
+                let itemPriceDomElement = createDomElement("Prix : " + item.price);
+                let itemQuantityDomElement = createDomElement("Quantité : " + item.quantity);
+
+                container.appendChild(itemNameDomElement);
+                container.appendChild(itemTypeDomElement);
+                container.appendChild(itemDescriptionDomElement);
+                container.appendChild(itemPriceDomElement);
+                container.appendChild(itemQuantityDomElement);
+
+                let separator = document.createElement("hr");
+                container.appendChild(separator);
+            }
+        }
+}
+
+////// au clic du bouton on récupère le texte saisi dans le champs texte
+button.addEventListener("click", function () {
+    let typedType = document.getElementById("rechercheParType").value;
+    // window.alert(typedType);
+    displayItemsByType(typedType);
+});
+
+
+/////appeler la fonction A avec le retour de la fonction B en paramètres
 
 
 
