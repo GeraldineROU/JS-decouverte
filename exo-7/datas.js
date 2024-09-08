adbf128a18379dc6b898f9cd6dc80a5ae04fb0c0
geraldine <geraldine.rouy@le-campus-numerique.fr>
2024-09-08 21:59:31 +0200

    exo 7, step 9 done

--- exo-7/datas.js
+++ exo-7/datas.js
@@ -1,57 +1,147 @@
 var jsonDatas=[
 	{
-		"name" :"Fiat Punto",
 		"type" : "car",
-		"description" : "Je suis une voiture",
-		"price" : 10000,
-		"quantity" : 2
+		"items": [
+			{
+				"name" :"Fiat Punto",
+				"description" : "Je suis une voiture",
+				"price" : 10000,
+				"quantity" : 2,
+				"contact" : {
+					"lastName" : "Dubois",
+					"firstName" : "Martin",
+					"address" : "1 Grande Rue 74000 Annecy"
+				}
+			}
+		]
+
 	},{
-		"name" :"Porsche 911",
 		"type" : "car",
-		"description" : "Je suis une belle voiture",
-		"price" : 80000,
-		"quantity" : 0
+		"items": [
+			{
+				"name" :"Porsche 911",
+				"description" : "Je suis une belle voiture",
+				"price" : 80000,
+				"quantity" : 0,
+				"contact" : {
+					"lastName" : "Scott",
+					"firstName" : "Micheal",
+					"address" : "126 Kellum Court, Scranton, PA 18510"
+				}
+			}
+		]
+
 	},{
-		"name" :"Villa sur la plage",
 		"type" : "house",
-		"description" : "Quelle belle vue",
-		"price" : 870000,
-		"quantity" : 1
+		"items": [
+			{
+				"name" :"Villa sur la plage",
+				"description" : "Quelle belle vue",
+				"price" : 870000,
+				"quantity" : 1,
+				"contact" : {
+					"lastName" : "Bauer",
+					"firstName" : "Jack",
+					"address" : "18166 San Fernando Road, Los Angeles, CA 90189"
+				}
+			}
+		]
+
 	},{
-		"name" :"Peugeot 205",
 		"type" : "car",
-		"description" : "Je suis une autre voiture",
-		"price" : 2000,
-		"quantity" : 2
+		"items": [
+			{
+				"name" :"Peugeot 205",
+				"description" : "Je suis une autre voiture",
+				"price" : 2000,
+				"quantity" : 2,
+				"contact" : {
+					"lastName" : "McGill",
+					"firstName" : "Jimmy",
+					"address" : "4317 Alture Ave NE, Alburqueque, New Mexico 87100"
+				}
+			}
+		]
+
 	},{
-		"name" :"Maison à la campagne",
 		"type" : "house",
-		"description" : "Vive le calme",
-		"price" : 170000,
-		"quantity" : 3
+		"items": [
+			{
+				"name" :"Maison à la campagne",
+				"description" : "Vive le calme",
+				"price" : 170000,
+				"quantity" : 3,
+				"contact" : {
+					"lastName" : "Locke",
+					"firstName" : "John",
+					"address" : "25164 Franklin Street, San Francisco, CA 94099"
+				}
+			}
+		]
+
 	},{
-		"name" :"Monopoly",
 		"type" : "game",
-		"description" : "",
-		"price" : 30,
-		"quantity" : 300
+		"items": [
+			{
+				"name" :"Monopoly",
+				"description" : "",
+				"price" : 30,
+				"quantity" : 300,
+				"contact" : {
+					"lastName" : "Geller",
+					"firstName" : "Monica",
+					"address" : "495 Grove Street, Greenwich Village, New York, NY 10014"
+				}
+			}
+		]
+
 	},{
-		"name" :"Mario Bros",
 		"type" : "videoGame",
-		"description" : "",
-		"price" : 30,
-		"quantity" : 200
+		"items": [
+			{
+				"name" :"Mario Bros",
+				"description" : "",
+				"price" : 30,
+				"quantity" : 200,
+				"contact" : {
+					"lastName" : "Day",
+					"firstName" : "Jessica",
+					"address" : "836 Traction Avenue, Los Angeles, CA 91210"
+				}
+			}
+		]
+
 	},{
-		"name" :"Place VIP concert Metallica",
 		"type" : "show",
-		"description" : "",
-		"price" : 800,
-		"quantity" : 10
+		"items": [
+			{
+				"name" :"Place VIP concert Metallica",
+				"description" : "",
+				"price" : 800,
+				"quantity" : 10,
+				"contact" : {
+					"lastName" : "Campbell",
+					"firstName" : "Wayne",
+					"address" : "586 Texhoma Avenue, Aurora, IL 60564"
+				}
+			}
+		]
+
 	},{
-		"name" :"Entrée au parc Astérix",
 		"type" : "show",
-		"description" : "",
-		"price" : 30,
-		"quantity" : 200
+		"items": [
+			{
+				"name" :"Entrée au parc Astérix",
+				"description" : "",
+				"price" : 30,
+				"quantity" : 200,
+				"contact" : {
+					"lastName" : "Raczek-Bernard",
+					"firstName" : "Elisabeth",
+					'address' : "19 rue Leverrier, 62880"
+				}
+			}
+		]
+
 	}
 ]
